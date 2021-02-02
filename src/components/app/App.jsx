5c376fd4a5a33c645e5509f92cb38182a185907e
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-02 17:19:15 -0500

    create getCharacters function to fetch character data

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     <>
       <Router>
         <Switch>
+          
           <Route 
             path="/" 
             exact 
---
+++ src/services/heyArnoldApi.js
@@ -0,0 +1,6 @@
+export const getCharacters = () => {
+  return fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
+    .then(res => res.json())
+    .then(console.log);
+};
+
