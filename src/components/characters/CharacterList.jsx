07d8f24cd69aafb11cf0271f3fa0eb1be07a9c3f
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-02 21:18:51 -0500

    adding styled components

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import Character from './Character';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const CharacterUl = styled.ul`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+const CharacterLi = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    border: 1px solid gray;
+    border-radius: 10px;
+    margin: 5px;
+    width: 250px;
+    height: 250px;
+    background: #FFCDB2;
+`;
 
 const CharacterList = ({ characters }) => {
-    const characterElements = characters.map(character => (
-        <Link to={`/detail/${character._id}`}>
-            <li key={character._id}>
-                <Character {...character} />
-            </li>
-        </Link>
-    ))
+  const characterElements = characters.map(character => (
+    <Link to={`/detail/${character._id}`}>
+        <CharacterLi key={character._id}>
+            <Character {...character} />
+        </CharacterLi>
+    </Link>
+  ));
     
-    return (
-        <ul>
-            {characterElements}
-        </ul>
-    )
-}
+  return (
+    <CharacterUl>
+      {characterElements}
+    </CharacterUl>
+  );
+};
 
 export default CharacterList;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MainHeader = styled.h1`
+    color: #B5838D;
+    text-align: center;
+`;
+
+const Paragraph = styled.p`
+    color: #B5838D;
+    font-size: 17px;
+    text-align: center;
+`;
+
 
 export default class Header extends Component {
   render() {
     return (
       <>
-        <h1>Welcome!</h1>
-        <p>Please select a character to learn more.</p>
+        <MainHeader>Welcome!</MainHeader>
+        <Paragraph>Please select a character to learn more.</Paragraph>
       </>
     );
   }
